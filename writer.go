271b84969d9ae6310fe0e1043d7b825e5c3a2a35
Milan Pavlik <milanpavlik@improbable.io>
2020-02-01 23:28:50 +0000

    Basic non-rotating implementation

--- writer.go
+++ writer.go
@@ -1,22 +1,97 @@
 package logrotate
 
-import "sync"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
 
 type Writer struct {
+	logger *log.Logger
 	// path to directory where log files will be stored
 	dir string
 
 	mux sync.Mutex
+
+	queue   chan []byte
+	pending sync.WaitGroup
+	closing chan struct{}
+	done    chan struct{}
+
+	f *os.File
 }
 
 func (w *Writer) Write(p []byte) (n int, err error) {
-	panic("implement me")
+	select {
+	case <-w.closing:
+		return 0, errors.Wrap(err, "writer is closing")
+	default:
+		w.pending.Add(1)
+		defer w.pending.Done()
+	}
+
+	w.queue <- p
+
+	return len(p), nil
 }
 
 func (w *Writer) Close() error {
-	panic("implement me")
+	close(w.closing)
+	w.pending.Wait()
+
+	close(w.queue)
+	<-w.done
+
+	if w.f != nil {
+		if err := w.f.Sync(); err != nil {
+			return errors.Wrap(err, "failed to sync current log file")
+		}
+
+		if err := w.f.Close(); err != nil {
+			return errors.Wrap(err, "failed to close current log file")
+		}
+	}
+
+	return nil
 }
 
-func New(dir string) *Writer {
-	return &Writer{dir: dir}
+func (w *Writer) listen() {
+	for b := range w.queue {
+		if w.f == nil {
+			path := filepath.Join(w.dir, fmt.Sprintf("%s.log", time.Now().UTC().Format(time.RFC3339)))
+			f, err := newFile(path)
+			if err != nil {
+				w.logger.Println(fmt.Sprintf("Failed to create new file at %v", path), err)
+			}
+			w.f = f
+		}
+
+		if _, err := w.f.Write(b); err != nil {
+			w.logger.Println("Failed to write to file.", err)
+		}
+	}
+
+	close(w.done)
+}
+
+func New(logger *log.Logger, dir string) *Writer {
+	w := &Writer{
+		logger:  logger,
+		dir:     dir,
+		queue:   make(chan []byte, 1024),
+		closing: make(chan struct{}),
+		done:    make(chan struct{}),
+	}
+
+	go w.listen()
+
+	return w
+}
+
+func newFile(path string) (*os.File, error) {
+	return os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
 }
--- writer_test.go
+++ writer_test.go
@@ -3,6 +3,8 @@ package logrotate
 import (
 	"github.com/stretchr/testify/require"
 	"io/ioutil"
+	"log"
+	"os"
 	"testing"
 )
 
@@ -10,10 +12,21 @@ func TestWriter(t *testing.T) {
 	dir, err := ioutil.TempDir("", "")
 	require.NoError(t, err)
 
-	w := New(dir)
+	defer func() {
+		require.NoError(t, os.RemoveAll(dir))
+	}()
+
+	logger := log.New(os.Stderr, "", log.LstdFlags)
+
+	w := New(logger, dir)
 
 	_, err = w.Write([]byte("foo"))
 	require.NoError(t, err)
 
-	require.NoError(t,w.Close(), "must close")
+	require.NoError(t, w.Close(), "must close")
+
+	files, err := ioutil.ReadDir(dir)
+	require.NoError(t, err)
+
+	require.Len(t, files, 1, "must write a single file")
 }
