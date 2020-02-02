0436dcc62ee257c231bf8f63721bcef5e565f51b
Milan Pavlik <milanpavlik@improbable.io>
2020-02-02 23:06:43 +0000

    doc.go

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Package logrotate implements application-side log rotation.
+//
+// logrotate supports the following modes:
+//	* size: rotate files when they reach particular size
+// 	* time: rotate files after a period of time elapses
+//
+package logrotate
